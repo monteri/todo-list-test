195b1b0cae3d4a30c49a3711b0523d5baaea93f4
monteri <36768631+monteri@users.noreply.github.com>
2022-10-30 17:21:59 +0200

    feat: update urls

--- src/urls.py
+++ src/urls.py
@@ -2,10 +2,10 @@ from src.controllers import get_todo_list, update_task,\
     delete_task, not_found, add_task
 
 APP_URLS = [
-    ('^/todo-list$', get_todo_list),
-    ('^/tasks/[0-9]+/update$', update_task),
-    ('^/tasks/[0-9]+/delete$', delete_task),
-    ('^/tasks$', add_task),
+    ('^/api/todo-list$', get_todo_list),
+    ('^/api/tasks/[0-9]+/update$', update_task),
+    ('^/api/tasks/[0-9]+/delete$', delete_task),
+    ('^/api/tasks$', add_task),
 ]
 
 FALLBACK_HANDLER = not_found
